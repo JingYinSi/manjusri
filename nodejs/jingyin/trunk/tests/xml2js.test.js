121610adeaca83f754c6f01dd3de0966a9c4e569
clx <clx@bd927045-edd8-42a3-850b-7abe76bd9c1c>
2016-10-13 01:44:09 +0000

    测试框架与已有代码重构

--- nodejs/jingyin/trunk/tests/xml2js.test.js
+++ nodejs/jingyin/trunk/tests/xml2js.test.js
@@ -2,10 +2,10 @@
  * Created by sony on 2016/9/26.
  */
 var js2xmlparser = require('js2xmlparser'),
-    XML = require('pixl-xml'),
+    parseStringToJs = require('xml2js').parseString,
     assert = require('assert');
 
-describe('pixl-xml', function () {
+describe('xml2js', function () {
     it('读取XML字符串', function () {
         var xmlstr = '<xml><ToUserName><![CDATA[gh_03858052afc1]]></ToUserName>'
             +'<FromUserName><![CDATA[o0ghywcUHxUdazzXEBvYPxU1PVPk]]></FromUserName>'
@@ -14,7 +14,10 @@ describe('pixl-xml', function () {
             +'<Event><![CDATA[subscribe]]></Event>'
             +'<EventKey><![CDATA[]]></EventKey>'
             +'</xml>';
-        var doc = XML.parse(xmlstr);
-        console.log(doc);
+        var json;
+        parseStringToJs(xmlstr, function (err, result) {
+            assert.equal(null, err);
+            console.log(JSON.stringify(result));
+        });
     });
 });
