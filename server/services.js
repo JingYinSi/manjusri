2173d74ba01e65855439475389e9f0d969977f3a
Denis Chen <hbdennischen@gmail.com>
2016-10-14 11:48:39 +0000

    已有代码重构

--- server/services.js
+++ server/services.js
@@ -6,13 +6,16 @@ var manjusri = require('./wechat/manjusri'),
     wechat = require('./wechat/wechat'),
     payment = require('./wechat/payment'),
     weixinModule = require('../modules/weixin');
+var payURL = "/jingyin/manjusri/pay/confirm";
 
 module.exports = {
     weixin: weixinModule({
         appId: "wx76c06da9928cd6c3",
         appSecret: "f4d498d87cf8641b83671a533c3999ec",
         mchId: "1364986702",
-        mchKey: "womendoushiwutaishanjingyinsidet"
+        mchKey: "womendoushiwutaishanjingyinsidet",
+        apiBaseURL: "https://api.weixin.qq.com/sns/oauth2/",
+        oauth2BaseURL: "https://open.weixin.qq.com/connect/oauth2/authorize"
     }),
 
     initRoutes: function (router) {
@@ -27,7 +30,7 @@ module.exports = {
             .get(accuvirtue.index)
             .post(accuvirtue.action);
 
-        router.route('/jingyin/manjusri/pay/confirm')
+        router.route(payURL)
             .get(payment.index);
 
         router.route('/jingyin/manjusri/pay/notify')
@@ -35,13 +38,13 @@ module.exports = {
     },
 
     sendPayUrl: function (res, payInfo) {
-        var payURL = "/jingyin/manjusri/pay/confirm?";
+        var url = payURL + '?';
         var index = 0;
         for (var k in payInfo) {
-            payURL += (index > 0) ? '&' + k + '=' + payInfo[k] : k + '=' + payInfo[k];
+            url += (index > 0) ? '&' + k + '=' + payInfo[k] : k + '=' + payInfo[k];
             index++;
         }
-        payURL = this.weixin.wrapRedirectURLByOath2Way(payURL);
-        res.end(payURL);
-    },
+        url = this.weixin.wrapRedirectURLByOath2Way(url);
+        res.end(url);
+    }
 }
