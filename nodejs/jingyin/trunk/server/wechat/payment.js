e57364ab6647775cacdae72e11046ede2378f4a5
clx <clx@bd927045-edd8-42a3-850b-7abe76bd9c1c>
2016-10-19 09:23:02 +0000

    域名已备案成功

--- nodejs/jingyin/trunk/server/wechat/payment.js
+++ nodejs/jingyin/trunk/server/wechat/payment.js
@@ -28,8 +28,7 @@ module.exports = {
                 logger.error(err);
                 return;
             }
-            weixin.prePay(openid, transId, transName, amount, function(err, payData){
-
+            weixin.prePay(openid, transId, transName, amount, function(payData){
                 payData.success = true;
                 logger.debug("准备前端H5支付参数:" + JSON.stringify(payData));
                 res.render('wechat/payment', payData);
