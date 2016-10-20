d8820cd6133bf3aafbed5902a571c6135f1e82a9
Denis Chen <hbdennischen@gmail.com>
2016-10-20 15:43:53 +0000

    正式公众账号调试成功

--- server/wechat/accvirtue.js
+++ server/wechat/accvirtue.js
@@ -8,13 +8,18 @@ module.exports = {
     //创建日行一善订单
     action: function (req, res) {
         var amount = req.body.amount;
-        VirtueModel.placeVirtue(amount, function (err, virtue) {
+        var trans = {
+            transName: '日行一善',
+            amount: amount
+        };
+        weixin.sendPayUrl(res, trans);
+        /*VirtueModel.placeVirtue(amount, function (err, virtue) {
             var trans = {
                 transId: virtue._id,
                 transName: '日行一善',
                 amount: amount
             };
             weixin.sendPayUrl(res, trans);
-        });
+        });*/
     }
 };
