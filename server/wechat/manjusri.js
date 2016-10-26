82892b235dc5f0f16e4e68d2f14efa3cd71d5830
Denis Chen <hbdennischen@gmail.com>
2016-10-26 13:32:14 +0000

    正式功能开发-建寺-随喜

--- server/wechat/manjusri.js
+++ server/wechat/manjusri.js
@@ -5,10 +5,6 @@ module.exports = {
         });
     },
 
-    index: function (req, res) {
-        res.render('wechat/manjusri');
-    },
-
     jiansi: function (req, res) {
         res.render('wechat/jiansi', {
             title: '建寺'
