94b670e5682aca48eee687a78595df68dc5760c2
clx <clx@bd927045-edd8-42a3-850b-7abe76bd9c1c>
2016-10-26 03:06:03 +0000

    正式功能开发-建寺

--- nodejs/jingyin/trunk/server/wechat/manjusri.js
+++ nodejs/jingyin/trunk/server/wechat/manjusri.js
@@ -2,10 +2,13 @@ module.exports = {
     home: function (req, res) {
         res.render('wechat/index');
     },
+
     index: function (req, res) {
         res.render('wechat/manjusri');
     },
+
     jiansi: function (req, res) {
-    res.render('wechat/jiansi');
-}
+        res.render('wechat/jiansi');
+    },
 };
+
