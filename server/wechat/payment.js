337f44eb7d84d503c37018381ed4808509b3c612
Denis Chen <hbdennischen@gmail.com>
2016-10-27 02:00:39 +0000

    正式功能开发-建寺-随喜

--- server/wechat/payment.js
+++ server/wechat/payment.js
@@ -40,7 +40,7 @@ module.exports = {
                     return;
                 }
                 var transId = virtue._id.toString();
-                weixin.prePay(trader, transId, trans.details.subject, trans.amount, function (err, payData) {
+                weixin.prePay(trader, transId, trans.details.subject, trans.amount*100, function (err, payData) {
                     if(err){
                         resWrap.setStatus(502);
                         return;
