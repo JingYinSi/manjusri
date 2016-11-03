5dd11c2789c492f8fe1a2868060df87154b82563
Denis Chen <hbdennischen@gmail.com>
2016-11-03 14:25:44 +0000

    正式功能开发-用户注册

--- modules/utils.js
+++ modules/utils.js
@@ -7,7 +7,7 @@ module.exports = {
     simpleGetJson: function (url, callback) {
         simpleget.concat(url, function (err, res, buf) {
             //TODO:检查是否出错
-            var obj = JSON.parse(buf.toString())
+            var obj = JSON.parse(buf.toString()); //TODO:这里少一个分号为何测试不能发现错误？
             callback(null, obj);
         });
     }
