a41a4752b2735c2dd2e00cb35e3599cab2719418
clx <clx@bd927045-edd8-42a3-850b-7abe76bd9c1c>
2016-11-20 14:06:25 +0000

    正式功能开发-功能完成

--- nodejs/jingyin/trunk/server/routes.js
+++ nodejs/jingyin/trunk/server/routes.js
@@ -37,8 +37,8 @@ module.exports = function (router) {
         .post(virtues.prepay);
     router.route('/jingyin/rest/virtues/:id')
         .put(virtues.paid);
-    router.route('/jingyin/rest/virtues')
-        .get(virtues.list);
+    /*router.route('/jingyin/rest/virtues')
+        .get(virtues.list);*/
 
     /*----------------------------业务系统------------------------------------------  */
     router.route('/jingyin/biz/parts/index')
