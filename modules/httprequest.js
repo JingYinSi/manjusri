f1a3e04afec4ca9e1b30163a12408b876a821158
Denis Chen <hbdennischen@gmail.com>
2016-11-29 04:31:44 +0000

    正式功能开发-重构-采用Promise

---
+++ modules/httprequest.js
@@ -0,0 +1,21 @@
+/**
+ * Created by clx on 2016/11/29.
+ */
+var simpleget = require('simple-get'),
+    Promise = require('bluebird');
+
+function HttpRequest(requestor){
+    this.requestor = requestor;
+}
+
+HttpRequest.prototype.concat = function(opt){
+    var self = this;
+    return new Promise(function (resolve, reject) {
+        self.requestor.concat(opt, function (err, res, data) {
+            if(err) return reject(err);
+            return resolve(data);
+        });
+    });
+}
+
+module.exports = new HttpRequest(simpleget);
