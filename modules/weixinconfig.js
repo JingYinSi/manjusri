d6fdb526061ce40a41a4c182c7b2b0d48d34b2f5
Denis Chen <hbdennischen@gmail.com>
2016-12-11 13:45:57 +0000

    获得用户信息，Scope为snsapi_userinfo

--- modules/weixinconfig.js
+++ modules/weixinconfig.js
@@ -92,8 +92,10 @@ WeixinConfig.prototype.generatePayData = function (prepayId) {
 }
 
 WeixinConfig.prototype.wrapRedirectURLByOath2Way = function (url) {
+    /*var wrapedUrl = oauth2BaseURL + "?appid=" + appId
+        + "&redirect_uri=" + url + "&response_type=code&scope=snsapi_base#wechat_redirect";*/
     var wrapedUrl = oauth2BaseURL + "?appid=" + appId
-        + "&redirect_uri=" + url + "&response_type=code&scope=snsapi_base#wechat_redirect";
+        + "&redirect_uri=" + url + "&response_type=code&scope=snsapi_userinfo#wechat_redirect";
     return wrapedUrl;
 }
 
