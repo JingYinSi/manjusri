6d29dfc29494e2ddd6cadde2c71d981b62ccab1e
Denis Chen <hbdennischen@gmail.com>
2017-03-17 10:04:09 +0000

    功德主

--- server/wechat/manjusri.js
+++ server/wechat/manjusri.js
@@ -109,35 +109,31 @@ module.exports = {
 
     lordVirtues: function (req, res) {
         var code = req.query.code;
-        if (code) {
-            logger.debug("out of redirect");
-            var resWrap = createResponseWrap(res);
-            return wx.weixinService.getOpenId(code)
-                .then(function (data) {
-                    var openid = data.openid;
-                    logger.debug("The openid is: " + openid);
-                    return UserModel.findOne({openid: openid});
-                })
-                .then(function (lord) {
-                    var data = {lord: lord};
-                    logger.debug("begin render wechat/lordVirtues with data:\n" + JSON.stringify(data));
-                    return res.render('wechat/lordVirtues', data);
-                })
-                .catch(function (err) {
-                    logger.debug("error:" + err);
-                    return resWrap.setError(400, null, err);
-                });
-        }
-        else {
+        if (!code) {
             logger.debug("begin redirect");
             var redirectUrl = wx.weixinConfig.wrapRedirectURLByOath2WayBaseScope(req.originalUrl);
-            //return res.redirect(redirectUrl);
             res.writeHead(302, {
                 'Location': redirectUrl
-                //add other headers here...
             });
             return res.end();
         }
+        logger.debug("out of redirect");
+        var resWrap = createResponseWrap(res);
+        return wx.weixinService.getOpenId(code)
+            .then(function (data) {
+                var openid = data.openid;
+                logger.debug("The openid is: " + openid);
+                return UserModel.findOne({openid: openid});
+            })
+            .then(function (lord) {
+                var data = {lord: lord};
+                logger.debug("begin render wechat/lordVirtues with data:\n" + JSON.stringify(data));
+                return res.render('wechat/lordVirtues', data);
+            })
+            .catch(function (err) {
+                logger.debug("error:" + err);
+                return resWrap.setError(400, null, err);
+            });
     }
 };
 
