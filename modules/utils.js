6fd3038e5f7c12a017276e7dd3ec40786bc1bab9
Denis Chen <hbdennischen@gmail.com>
2017-03-20 08:53:37 +0000

    功德主

--- modules/utils.js
+++ modules/utils.js
@@ -10,5 +10,42 @@ module.exports = {
             var obj = JSON.parse(buf.toString()); //TODO:这里少一个分号为何测试不能发现错误？
             callback(null, obj);
         });
+    },
+
+    dateUtils: {
+        maxYestoday: function (day) {
+            var theDay = day ? day : new Date();
+            return new Date(theDay.setHours(0,0,0,0) - 1);
+        },
+
+        minTomorrow: function (day) {
+            var theDay = day ? day : new Date();
+            return new Date( theDay.setHours(23, 59, 59, 999) + 1);
+        },
+
+        minToday: function (day) {
+            var theDay = day ? day : new Date();
+            return new Date(theDay.setHours(0,0,0,0));
+        },
+
+        maxToday: function (day) {
+            var theDay = day ? day : new Date();
+            return new Date(theDay.setHours(23, 59, 59, 999));
+        },
+
+        minThisMonth: function (day) {
+            var theDay = day ? day : new Date();
+            var thisyear = theDay.getFullYear();
+            var thismonth = theDay.getMonth();
+            return new Date(thisyear, thismonth, 1);
+        },
+
+        maxThisMonth: function (day) {
+            var theDay = day ? day : new Date();
+            var thisyear = theDay.getFullYear();
+            var thismonth = theDay.getMonth();
+            return new Date(new Date(thisyear, thismonth + 1, 1) - 1);
+        }
     }
+
 }
