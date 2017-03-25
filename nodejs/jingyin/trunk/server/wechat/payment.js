868f5dced2e641055f0facc9b412c5d3af334dec
clx <clx@bd927045-edd8-42a3-850b-7abe76bd9c1c>
2017-03-25 03:18:11 +0000

    功德主和用户登录基本完成

--- nodejs/jingyin/trunk/server/wechat/payment.js
+++ nodejs/jingyin/trunk/server/wechat/payment.js
@@ -2,6 +2,7 @@ var wx = require('../weixin').weixinService,
     virtues = require('../modules/virtues'),
     users = require('../modules/users'),
     Promise = require('bluebird'),
+    redirects = require('./redirects');
     responseWrapFactory = require('../../modules/responsewrap');
 
 var log4js = require('log4js');
@@ -10,6 +11,42 @@ var logger = log4js.getLogger();
 
 module.exports = {
     pay: function (req, res) {
+        var resWrap = responseWrapFactory(res);
+        var virtueId = req.query.virtue;
+        if (!virtueId) {
+            return resWrap.setError(400, null, new Error('virtue is not found in query'));
+        }
+        var openId, subjectName, amount;
+        var sess = req.session;
+        if (!sess.user) return redirects.toLogin(req, res);
+        openId = sess.user.openid;
+
+        var tasks = [
+            virtues.findNewVirtueById(virtueId)
+                .then(function (doc) {
+                    if(!doc)
+                        return Promise.reject(new Error('The virtue[id=' + virtueId + '] is not found'));
+                    subjectName = doc.subject.name;
+                    amount = doc.amount;
+                })];
+        return Promise.all(tasks)
+            .then(function () {
+                return wx.prepay(openId, virtueId, subjectName, amount);
+            })
+            .then(function (payData) {
+                logger.debug("Pay data to be sent to H5:" + JSON.stringify(payData));
+                resWrap.render('wechat/payment', {
+                    openId: openId,
+                    virtue: virtueId,
+                    payData: payData
+                });
+            })
+            .catch(function (err) {
+                return resWrap.setError(400, null, err);
+            });
+    },
+
+    /*pay: function (req, res) {
         var resWrap = responseWrapFactory(res);
         var code = req.query.code;
         if (!code) {
@@ -51,7 +88,7 @@ module.exports = {
             .catch(function (err) {
                 return resWrap.setError(400, null, err);
             });
-    },
+    },*/
 
     result: function (req, res) {
         res.render('wechat/paymentResult');
