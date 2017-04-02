2860d67301f1378d5030e2d83a4901974b2f8e5d
Denis Chen <hbdennischen@gmail.com>
2017-04-02 14:06:16 +0000

    cluster

--- modules/utils.js
+++ modules/utils.js
@@ -48,6 +48,19 @@ module.exports = {
         }
     },
 
+    round: function (num, scale) {
+        if (!("" + num).includes("e")) {
+            return +(Math.round(num + "e+" + scale) + "e-" + scale);
+        } else {
+            var arr = ("" + num).split("e");
+            var sig = ""
+            if (+arr[1] + scale > 0) {
+                sig = "+";
+            }
+            return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+        }
+    },
+
     range: {
         create: function (rangeArray) {
             var result = [];
--- querys.js
+++ querys.js
@@ -1,11 +1,54 @@
 /**
  * Created by clx on 2017/3/28.
  */
+
+var byProvicesAndCities = [
+    {
+        $match: {
+            state: 'payed',
+            timestamp: {
+                $gte: new Date(new Date().setHours(0, 0, 0, 0)),
+                $lte: new Date(new Date().setHours(23, 59, 59, 999))
+            }
+        }
+    },
+    {$lookup: {from: "users", localField: "lord", foreignField: "_id", as: "lorddoc"}},
+    {$project: {"lorddoc.province": 1, "lorddoc.city": 1, amount: 1, timestamp: 1}},
+    {
+        $facet: {
+            total: [
+                {
+                    $group: {
+                        _id: null,
+                        count: {$sum: 1}, sum: {$sum: "$amount"}
+                    }
+                }
+            ],
+            byProvinces: [
+                {
+                    $group: {
+                        _id: "$lorddoc.province",
+                        count: {$sum: 1}, sum: {$sum: "$amount"}
+                    }
+                }
+            ],
+            byCities: [
+                {
+                    $group: {
+                        _id: {province: "$lorddoc.province", city: "$lorddoc.city"},
+                        count: {$sum: 1}, sum: {$sum: "$amount"}
+                    }
+                }
+            ]
+        }
+    }
+];
+
 db.virtues.aggregate([
     {
         $match: {state: 'payed'}
     },
-    {$group: {_id: "$lord", count: { $sum: 1 }, sum: {$sum: "$amount"}}},
+    {$group: {_id: "$lord", count: {$sum: 1}, sum: {$sum: "$amount"}}},
     {
         $sort: {sum: -1}
     }
