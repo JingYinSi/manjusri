0df869d903ece131b6db9f9ece956645e77c45b7
clx <clx@bd927045-edd8-42a3-850b-7abe76bd9c1c>
2017-04-20 08:38:40 +0000

    完成支付成功分享

--- nodejs/jingyin/trunk/server/wechat/payment.js
+++ nodejs/jingyin/trunk/server/wechat/payment.js
@@ -79,9 +79,9 @@ module.exports = {
         };
 
         var host = "http://jingyintemple.top";
-        //var relativeUrl = req.url;
-        var relativeUrl = req.path;
+        var relativeUrl = req.url;
         var url = host + relativeUrl;
+        var url = "http://jingyintemple.top/jingyin/manjusri/pay/notify";
 
         /*return wx.generateShareConfig(url, function (shareConfig) {
             viewdata.shareConfig = shareConfig;
