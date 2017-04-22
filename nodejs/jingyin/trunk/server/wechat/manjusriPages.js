64948f1ba89fac2c63ca699daf38646dbd9e4713
clx <clx@bd927045-edd8-42a3-850b-7abe76bd9c1c>
2017-04-22 10:36:33 +0000

    完成支付成功分享

--- nodejs/jingyin/trunk/server/wechat/manjusriPages.js
+++ nodejs/jingyin/trunk/server/wechat/manjusriPages.js
@@ -67,7 +67,7 @@ module.exports = {
             },
             share: {
                 title: '静音寺.文殊禅林', // 分享标题
-                desc: '传承正法，培养僧才，实修实证，秉承宗风，效仿古贤', // 分享描述
+                desc: '传承正法，培养僧才，实修实证，弘扬人间佛教，共建人间净土！', // 分享描述
                 link: linkages.getLink('home'),  // 分享链接
                 imgUrl: wx.weixinConfig.getShareLogoImage(), // 分享图标
             },
