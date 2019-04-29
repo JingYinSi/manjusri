7504c31bc285e6a7afa2edf964c25a467c708547
clx1608@qq.com <hbdennischen@outlook.com>
2019-04-29 21:37:52 +0800

    test wx login

--- server/rests/WechatSignin.js
+++ server/rests/WechatSignin.js
@@ -16,48 +16,7 @@ function signin(req, res) {
         return res.status(errCode).end()
     }
     
-    return wx.getOpenId(code)
-        .then(function (data) {
-            const sess = req.session;
-            sess.user = {...data};
-            logger.debug("session is built, and context of current session is:" + JSON.stringify(sess, null, 2));
-            return res.redirect(`http://dev.jingyintemple.top/index.html#${req.query.url}?code=${req.query.code}`)
-            // return sess.user
-            //TODO:全局性地缓存refresh_token，在我的基础资料处可从该全局缓存中获得accesstoken, 以同步用户资料
-            //sess.refresh_token = data.refresh_token;
-
-            // return usersModule.findByOpenid(openid);
-        })
-        /* .then(function (user) {
-            if (!user) {
-                errCode = 500;
-                return wx.weixinService.getUserInfoByOpenIdAndToken(accessToken, openid)
-                    .then(function (userInfo) {
-                        var data = {
-                            name: userInfo.nickname,
-                            openid: userInfo.openid,
-                            img: userInfo.headimgurl,
-                            city: userInfo.city,
-                            province: userInfo.province,
-                            sex: userInfo.sex,
-                            subscribe: userInfo.subscribe_time
-                        }
-                        return usersModule.registerUser(data)
-                            .then(function (user) {
-                                return redirects.toProfile(openid, req, res);
-                            });
-                    });
-            } else {
-                //TODO:访问微信用户资料补充当前不足的用户信息
-                var redirectToUrl = req.session.redirectToUrl;
-                return redirectToUrl ? res.redirect(redirectToUrl) :
-                    redirects.toHome(req, res);
-            }
-        }) */
-        .catch(function (err) {
-            logger.debug("error:" + err);
-            res.status(500).end()
-        });
+    return res.redirect(`http://dev.jingyintemple.top/index.html#${req.query.url}?code=${req.query.code}`)
 }
 
 module.exports = {
--- server/rests/WechatUser.js
+++ server/rests/WechatUser.js
@@ -1,6 +1,55 @@
+const wx = require('../weixin').weixinService,
+    logger = require('@finelets/hyper-rest/app/Logger')
+
 function sessUser(req, res) {
-    if(!req.session || !req.session.user) return res.status(400).end()
-    return res.json(req.session.user)
+    let code = req.query.code
+    if (!code) {
+        return res.status(403).end()
+    }
+
+    return wx.getOpenId(code)
+        .then(function (data) {
+            const sess = req.session;
+            sess.user = {...data};
+            logger.debug("session is built, and context of current session is:" + JSON.stringify(sess, null, 2));
+            // 
+            return res.json(sess.user)
+            // return sess.user
+            //TODO:全局性地缓存refresh_token，在我的基础资料处可从该全局缓存中获得accesstoken, 以同步用户资料
+            //sess.refresh_token = data.refresh_token;
+
+            // return usersModule.findByOpenid(openid);
+        })
+        /* .then(function (user) {
+            if (!user) {
+                errCode = 500;
+                return wx.weixinService.getUserInfoByOpenIdAndToken(accessToken, openid)
+                    .then(function (userInfo) {
+                        var data = {
+                            name: userInfo.nickname,
+                            openid: userInfo.openid,
+                            img: userInfo.headimgurl,
+                            city: userInfo.city,
+                            province: userInfo.province,
+                            sex: userInfo.sex,
+                            subscribe: userInfo.subscribe_time
+                        }
+                        return usersModule.registerUser(data)
+                            .then(function (user) {
+                                return redirects.toProfile(openid, req, res);
+                            });
+                    });
+            } else {
+                //TODO:访问微信用户资料补充当前不足的用户信息
+                var redirectToUrl = req.session.redirectToUrl;
+                return redirectToUrl ? res.redirect(redirectToUrl) :
+                    redirects.toHome(req, res);
+            }
+        }) */
+        .catch(function (err) {
+            logger.debug("error:" + err);
+            res.status(500).end()
+        });
 }
 
 module.exports = {
